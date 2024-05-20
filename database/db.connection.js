410e4d36a12612bf2856878343c7c160f424c355
KHMalik03 <khalfamalik2003@gmail.com>
2024-05-20 22:10:25 +0100

    code restructured using MVC model

---
+++ database/db.connection.js
@@ -0,0 +1,12 @@
+const sql = require('sql');
+sql.setDialect(/* 'possible dialects: mssql, mysql, postgres (default), sqlite' */);
+
+let connection;
+
+exports.connect=async()=>{
+/* connection string */
+}
+
+exports.close=()=>{
+/* close connection */
+}
--- main.js
+++ main.js
@@ -1,14 +1,5 @@
 const express = require('express');
 const app = express();
-const sql = require('sql');
-sql.setDialect(/* 'possible dialects: mssql, mysql, postgres (default), sqlite' */);
-
-
-/* define table with one table(exemple)*/
-const user = sql.define({
-    name : 'table name',
-    columns : ['col 1','col 2','col 3']
-})
 
 /* setting up the view engine */
 app.set('view engine', 'ejs');
@@ -18,19 +9,6 @@ app.use('/public', express.static('public'));
 
 
 
-/* setting route for user search and filtring */
-app.get('/users', (req, res) => {
-    res.render('userPageFileName', {
-        user: user
-    })
-})
-
-/* setting up route for searching by name */
-app.get('/user/:id/delete',(req,res)=>{
-    let userId = req.params.id;
-    let user = user.colname.find(user=>user.id ==userId);
-
-    if(user){
-        /* DELETE FROM USER WHERE userId=ID */
-    }
-})
+app.listen(3000, () => {
+    console.log("Server started at: http://localhost:3000");
+}) 
